c35cfae0ef3c1e99c9711e1b3a039a3711c84de8
pravinfer <pravinfer14@gmail.com>
2022-01-10 17:30:42 +0530

    done

---
+++ src/org/tcs/Employee.java
@@ -0,0 +1,12 @@
+package org.tcs;
+
+public class Employee {
+	private void add() {
+		System.out.println("add");
+	}
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		e.add();
+	}
+
+}
